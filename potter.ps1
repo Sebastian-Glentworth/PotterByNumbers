8d381f5333620227c847100e409466d6ef5fb900
Sebastian Glentworth <seb@stian.co.uk>
2019-03-23 14:42:25 +0000

    added sales and words to output

--- potter.ps1
+++ potter.ps1
@@ -1,8 +1,19 @@
 $potterInfo = (Get-Content './potter.json' | Out-String | ConvertFrom-Json)
 
-$runningTotal = 0
+$runningTotalSales = 0
+$runningTotalWords = 0
+$runningTotalWordsSold = 0
 $potterInfo.books | ForEach-Object{ 
-    Write-Output ("{0}: {1:N0}" -f $_.Title, ($_.Sales * $_.Words))
-    $runningTotal += ($_.Sales * $_.Words)
+    Write-Output ("{0}:" -f $_.Title)
+    Write-Output ("  Sales: {0:N0}" -f $_.Sales)
+    Write-Output ("  Words: {0:N0}" -f $_.Words)
+    Write-Output ("  Words Sold: {0:N0}" -f ($_.Sales * $_.Words))
+
+    $runningTotalSales += $_.Sales
+    $runningTotalWords += $_.Words
+    $runningTotalWordsSold += ($_.Sales * $_.Words)
 }
-Write-Output ("Total: {0:N0}" -f $runningTotal)
+Write-Output ("Total: ")
+Write-Output ("  Sales: {0:N0}" -f $runningTotalSales)
+Write-Output ("  Words: {0:N0}" -f $runningTotalWords)
+Write-Output ("  Words Sold: {0:N0}" -f $runningTotalWordsSold)
